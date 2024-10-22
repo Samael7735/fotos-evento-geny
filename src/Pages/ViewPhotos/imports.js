08e4f59f740fe7efbc81842a921bc43ce91d5057
Samuel Sena <92332763+Samael7735@users.noreply.github.com>
2024-10-22 17:01:07 -0300

    Colocando imagens para testar hehe

--- src/Pages/ViewPhotos/imports.js
+++ src/Pages/ViewPhotos/imports.js
@@ -1,6 +1,8 @@
+import Foto01 from "../../assets/Fotos/Foto01.jpeg";
+import Foto02 from "../../assets/Fotos/Foto02.jpeg";
 
 const photoImports = [
-  
+  Foto01, Foto02
 ];
 
 export default photoImports;
