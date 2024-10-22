ea734d69b82006d1bde0e853fd1d89c64b287d64
Samuel Sena <92332763+Samael7735@users.noreply.github.com>
2024-10-22 15:14:37 -0300

    Responsivo

--- src/Components/CardPhotos/CardPhotos.jsx
+++ src/Components/CardPhotos/CardPhotos.jsx
@@ -1,9 +1,22 @@
-import { Box, Image} from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
-export const CardPhotos = (props)=>{
-    return(
-        <Box w='100%' h='100%' borderRadius='10px' _hover={{border:'solid 1px', borderColor:'yellow'}}>
-            <Image w='100%' h='100%' borderRadius='10px' objectFit='cover'  src={props.src} alt='Imagens convidados'/>
+export const CardPhotos = (props) => {
+    return (
+        <Box
+            borderRadius='10px'
+            _hover={{ border: 'solid 1px', borderColor: 'yellow' }}
+            overflow='hidden' // Para evitar que a imagem exceda o border-radius
+            maxH='200px'
+        >
+            <Image 
+                w='100%' 
+                h='100%'
+                maxH='200px'
+                borderRadius='10px' 
+                objectFit='cover'  
+                src={props.src} 
+                alt='Imagens convidados' 
+            />
         </Box>
-    )
-}
+    );
+};
--- src/Pages/ViewPhotos2/ViewPhotos2.jsx
+++ src/Pages/ViewPhotos2/ViewPhotos2.jsx
@@ -32,72 +32,27 @@ export const ViewPhotos2 = () => {
     const generateImageLinks = (photoNames) => {
         const baseUrl = "https://raw.githubusercontent.com/Samael7735/fotos-evento-geny/main/src/assets/Fotos"; // URL base para as imagens
         const links = photoNames.map(name => `${baseUrl}/${name}`); // Cria links completos
-        console.log('Generated image links:', links); // Debug: mostra os links gerados
         return links;
     };
 
     useEffect(() => {
         const storedLinks = localStorage.getItem('imageLinks');
-        console.log('Stored links from localStorage:', storedLinks); // Debug: mostra os links armazenados
 
         if (storedLinks) {
-            // Se os links estão armazenados, parseia e atualiza o estado
             setImageLinks(JSON.parse(storedLinks));
-            console.log('Loaded links from localStorage:', JSON.parse(storedLinks)); // Debug: mostra os links carregados
         } else {
-            // Se não há links armazenados, gera novos links
             const photoNames = photoImports.map(photo => photo.replace(/"/g, '')); // Limpa aspas do array importado
             const newLinks = generateImageLinks(photoNames); // Cria novos links
-
             setImageLinks(newLinks); // Atualiza o estado com os novos links
             localStorage.setItem('imageLinks', JSON.stringify(newLinks)); // Armazena os links no local storage
         }
     }, []); // Este efeito roda apenas uma vez na montagem
 
-    // Este useEffect monitora mudanças em photoImports
-    useEffect(() => {
-        if (photoImports.length > 0) {
-            console.log('photoImports has changed:', photoImports); // Debug: mostra as mudanças em photoImports
-            const photoNames = photoImports.map(photo => photo.replace(/"/g, '')); // Limpa aspas do array importado
-            const newLinks = generateImageLinks(photoNames); // Cria novos links
-
-            setImageLinks(newLinks); // Atualiza o estado com os novos links
-            localStorage.setItem('imageLinks', JSON.stringify(newLinks)); // Armazena os links no local storage
-            console.log('Updated image links:', newLinks); // Debug: mostra os novos links gerados
-        }
-    }, [photoImports]); // Executa sempre que photoImports muda
-
     const handleOpenModal = (foto) => {
         setSelectedPhoto(foto);
         onOpen();
-    };
-
-    const handleDownload = async () => {
-        if (!selectedPhoto) {
-            console.error('Nenhuma foto selecionada para download');
-            return;
-        }
-
-        try {
-            const response = await fetch(selectedPhoto); // Faz uma requisição para obter a imagem
-            const blob = await response.blob(); // Converte a resposta em um Blob
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob); // Cria um URL para o Blob
-            link.download = selectedPhoto.split('/').pop(); // Nome do arquivo
-            document.body.appendChild(link);
-            link.click(); // Simula o clique para iniciar o download
-            document.body.removeChild(link); // Remove o link do DOM após o download
-            URL.revokeObjectURL(link.href); // Libera a memória do Blob
-            onClose(); // Fecha o modal após o download
-        } catch (error) {
-            console.error('Erro ao baixar a foto:', error);
-        }
-    };
-
-    const vazio = imageLinks.length <= 0 ? "repeat(auto-fit, minmax(150px, 500px));" : "repeat(auto-fit, minmax(150px, 180px));";
-    const margin = imageLinks.length <= 0 ? "0px" : "10%";
-    const display = imageLinks.length <= 0 ? 'none' : 'block';
-
+    }
+    
     return (
         <>
             <Header />
@@ -113,7 +68,7 @@ export const ViewPhotos2 = () => {
                         Melhores momentos 🍻
                     </Text>
                     <Text
-                        mb={margin}
+                        mb='10%'
                         color="textColor.200"
                         textAlign="center"
                         fontSize="clamp(1.2rem, 1vw, 2vw)"
@@ -121,36 +76,49 @@ export const ViewPhotos2 = () => {
                     >
                         Baixe suas fotos favoritas!
                     </Text>
-                    <Grid templateColumns={vazio} gap={3} mb='10%'>
-                        {imageLinks.length <= 0 ? (
+                    <Grid 
+                        w='100%' 
+                        templateColumns='repeat(auto-fit, minmax(150px, 1fr))' // Use o valor de templateColumns dinâmico
+                        gap={3} 
+                        mb='10%'
+                    >
+                    {imageLinks.length <= 0 ? (
+                        <GridItem
+                            key="ghost"
+                            borderRadius="10px"
+                            maxH="500px"
+                            objectFit='contain'
+                            display='flex' 
+                            alignItems='center' 
+                            justifyContent='center' 
+                            flexDir='column'
+                        >
+                            <Image src={Ghost} alt='fantasminha' />
+                            <Text mb='5%' textAlign='center' color='orange'>
+                                Ainda não há fotos, volte mais tarde e atualize a página
+                            </Text>
+                            <Text textAlign='center' color='white'>
+                                <NavLink to='/'>Inicio</NavLink>
+                            </Text>
+                        </GridItem>
+                    ) : (
+                        imageLinks.map((foto) => (
                             <GridItem
-                                key="ghost"
+                                onClick={() => handleOpenModal(foto)}
+                                key={foto} // Use o link da foto como key
                                 borderRadius="10px"
-                                w="100%"
-                                maxH="500px"
-                                objectFit='contain'
-                                display='flex' alignItems='center' justifyContent='center' flexDir='column'
+                                h="200px"
+                                maxH='200px'
                             >
-                                <Image src={Ghost} alt='fantasminha' />
-                                <Text mb='5%' textAlign='center' color='orange'>Ainda não há fotos, volte mais tarde e atualize a página</Text>
-                                <Text textAlign='center' color='white'><NavLink to='/'>Inicio</NavLink></Text>
+                                <CardPhotos src={foto} />
                             </GridItem>
-                        ) : (
-                            imageLinks.map((foto) => (
-                                <GridItem
-                                    onClick={() => handleOpenModal(foto)}
-                                    key={foto} // Use o link da foto como key
-                                    borderRadius="10px"
-                                    w="100%"
-                                    h="200"
-                                    maxH="200px"
-                                >
-                                    <CardPhotos src={foto} />
-                                </GridItem>
-                            ))
-                        )}
+                        ))
+                    )}
                     </Grid>
-                    <Link display={display} href="#topo" textAlign='center' fontSize='1.3rem' color='orange'>Voltar ao topo</Link>
+
+                    <Link display={imageLinks.length <= 0 ? 'none' : 'block'} href="#topo" textAlign='center' fontSize='1.3rem' color='orange'>
+                        Voltar ao topo
+                    </Link>
                 </Box>
             </BoxSection>
             <Modal isOpen={isOpen} onClose={onClose} size='xl'>
@@ -165,7 +133,7 @@ export const ViewPhotos2 = () => {
                         <Text fontWeight='500' fontSize='1.3rem'>Boteco SME 02/10/2024</Text>
                     </ModalBody>
                     <ModalFooter display='flex' justifyContent='space-between'>
-                        <Button w='49%' colorScheme="blue" onClick={handleDownload}>
+                        <Button w='49%' colorScheme="blue" onClick={() => {}}>
                             Baixar Foto
                         </Button>
                         <Button w='49%' colorScheme="yellow" onClick={onClose}>
