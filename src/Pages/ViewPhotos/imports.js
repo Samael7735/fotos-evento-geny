13fa3181a504af49cdd908c0a56e7d10ffb2137d
Samuel Sena <92332763+Samael7735@users.noreply.github.com>
2024-10-22 17:11:57 -0300

    Eu tinha rodado o comando errado kk vmos ver agora

--- src/Pages/ViewPhotos/imports.js
+++ src/Pages/ViewPhotos/imports.js
@@ -1,8 +1,5 @@
-import Foto01 from "../../assets/Fotos/Foto01.jpeg";
-import Foto02 from "../../assets/Fotos/Foto02.jpeg";
-
 const photoImports = [
-  Foto01, Foto02
+  
 ];
 
 export default photoImports;
